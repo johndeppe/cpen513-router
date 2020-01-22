d97642b95277545086ca3c6552a634569234ea3e
John Deppe <deppe@ece.ubc.ca>
2020-01-22 15:33:01 -0800

    parser doesn't need an object

--- infile_parser.py
+++ infile_parser.py
@@ -1,36 +1,45 @@
 class Layout:
-    def __init__(self, name, size, obstacles, wires):
+    def __init__(self, name=None, size=(1,1), obstacles=[], wires=[]):
         self.name = name # from filename
         self.size = size # (x,y) tuple
         self.obstacles = obstacles # list of (x,y) tuples
         self.wires = wires # list of lists of (x,y) tuples
 
-class InfileParser:
-    def __init__(self, filename):
-        with open('benchmarks/'+filename, 'rt') as f:
-            # TODO add some parsing exception checking
-            # TODO check for non-negative integers
-            size = tuple( map( int, f.readline().split() ) )
-            # TODO make sure len(size) == 2
-            num_obstacles = int( f.readline() )
-            obstacles = []
-            for _ in range(num_obstacles):
-                obstacle = tuple( map( int, f.readline().split() ) )
-                obstacles.append(obstacle)
-            num_wires = int( f.readline() )
-            wires = []
-            for wire_index in range(num_wires):
-                wires.append([])
-                # iterate over the line, extracting pins
-                it = iter( map( int, f.readline().split() ) )
-                # https://stackoverflow.com/questions/16789776/iterating-over-two-values-of-a-list-at-a-time-in-python
-                num_pins = next(it)
-                for val in it:
-                    x = val
-                    y = next(it)
-                    wires[wire_index].append( (x,y) )
-            print("size", size)
-            print("obstacles", obstacles)
-            print("wires", wires)
+    def console_print(self):
+        print(self.name)
+        print(self.size)
+        print(self.obstacles)
+        print(self.wires)
+        #for y in range(size[1]):
+            #for x in range(size[0]):
+                #print('b', end='')
 
-_ = InfileParser('example.infile')
+def parseInputfile(filename):
+    with open('benchmarks/'+filename, 'rt') as f:
+        name = filename # TODO strip extension
+        # TODO add some parsing exception checking
+        # TODO check for non-negative integers
+        size = tuple( map( int, f.readline().split() ) )
+        # TODO make sure len(size) == 2
+        num_obstacles = int( f.readline() )
+        obstacles = []
+        for _ in range(num_obstacles):
+            obstacle = tuple( map( int, f.readline().split() ) )
+            obstacles.append(obstacle)
+        num_wires = int( f.readline() )
+        wires = []
+        for wire_index in range(num_wires):
+            wires.append([])
+            # iterate over the line, extracting pins
+            it = iter( map( int, f.readline().split() ) )
+            # https://stackoverflow.com/questions/16789776/iterating-over-two-values-of-a-list-at-a-time-in-python
+            # TODO check for odd number of pin coordinates
+            num_pins = next(it)
+            for val in it:
+                x = val
+                y = next(it)
+                wires[wire_index].append( (x,y) )
+        return Layout(name, size, obstacles, wires)
+
+my_layout = parseInputfile('example.infile')
+my_layout.console_print()
