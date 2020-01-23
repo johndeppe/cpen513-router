fc82ebebe78c08f7176e31c39f9e03ad4ac60003
John Deppe <deppe@ece.ubc.ca>
2020-01-22 16:30:00 -0800

    print grid to console

--- router.py
+++ router.py
@@ -4,15 +4,45 @@ class Layout:
         self.size = size # (x,y) tuple
         self.obstacles = obstacles # list of (x,y) tuples
         self.wires = wires # list of lists of (x,y) tuples
+        # points dict keeps track of our routing
+        # Keys:   (x, y) tuples
+        # Values: non-negative are wires (of that number's index)
+        # Values: not present is empty, -1 is obstacle
+        self.grid = {}
+        for o in obstacles:
+            self.grid[o] = -1
+        for wire_ndx in range( len(wires) ):
+            for pin in wires[wire_ndx]:
+                self.grid[pin] = wire_ndx
 
-    def console_print(self):
-        print(self.name)
-        print(self.size)
-        print(self.obstacles)
-        print(self.wires)
-        #for y in range(size[1]):
-            #for x in range(size[0]):
-                #print('b', end='')
+    def print_values(self):
+        print('name', self.name)
+        print('size', self.size)
+        print('obstacles', self.obstacles)
+        print('wires', self.wires)
+
+    def print_grid(self):
+        def print_x_guide():
+            # print x-coordinate guide
+            print('\n  ', end='')
+            for x in range(self.size[0]):
+                print( str(x%10), end='')
+            print('\n')
+
+        print_x_guide()
+        for y in range(self.size[1]):
+            print(str(y%10)+' ', end='') # left y-coord guide
+            # print grid
+            for x in range(self.size[0]):
+                if self.grid.get( (x,y) ) == None :
+                    print('.', end='')
+                elif self.grid.get( (x,y) ) == -1 :
+                    print('X', end='')
+                else:
+                    # TODO fix for more than 10 nets
+                    print( self.grid.get( (x,y) )%10, end='' )
+            print( ' '+str(y%10) ) # right y-coord guide
+        print_x_guide()
 
 def parseInputfile(filename):
     with open('benchmarks/'+filename, 'rt') as f:
@@ -42,4 +72,5 @@ def parseInputfile(filename):
         return Layout(name, size, obstacles, wires)
 
 my_layout = parseInputfile('example.infile')
-my_layout.console_print()
+my_layout.print_values()
+my_layout.print_grid()
